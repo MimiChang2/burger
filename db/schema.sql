f4cb4e3e2bdbe6c7bf0a926b7fc11453db0cefd1
Mimi Chang <mimichang94@yahoo.com>
2017-12-12 20:00:53 +0000

    folders created

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+-- phpmyadmin-ctl install
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(30),
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY(id)
+),
+
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+-- date: a TIMESTAMP.
+
---
+++ models/burger.js
@@ -0,0 +1,4 @@
+//import ORM
+
+// Create the code that will call the ORM functions using burger specific input for the ORM.
+// Export at the end of the burger.js file.
